a5a1d4f038e8a969e7cd5ea554e5a76d7773e5d7
Paramjeet <paramjeetnpradhan@gmail.com>
2023-12-21 17:11:44 +0530

    chnaged model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
 
-    {   
+    {   username:{
+            type: String,
+            required: true,
+        }, 
+       
         name: {
             type: String,
             required: true,
@@ -15,7 +19,13 @@ const UserSchema = new mongoose.Schema(
             type: String,
             required: true,
             unique: true,
-        }
+        },
+        coverPic:{
+             type: String,
+        },
+        city:{
+             type: String,
+        },
 
     }
 
